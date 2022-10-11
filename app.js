cfcde576b136d23ccf32ba42b4fce742089dc2e8
Yan Pablo <93152439+YanppSilva@users.noreply.github.com>
2022-10-11 11:49:59 -0300

    atualização da página
    adções ao css e ao js

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const botaoMenu = document.querySelector('.cabecalho__menu')
-const menu = document.querySelector('.menu-lateral') 
+const menu = document.querySelector('.menu-lateral')
+const list = document.querySelectorAll('.menu-lateral__link') 
 
 botaoMenu.addEventListener('click', () => (
     menu.classList.toggle('menu-lateral--ativo')
 ))
+
+function activeList() {
+    list.forEach((item) =>
+    item.classList.remove('menu-lateral__link--ativo'));
+    this.classList.add('menu-lateral__link--ativo')
+}
+
+list.forEach((item) =>
+item.addEventListener('click', activeList))
+
+
